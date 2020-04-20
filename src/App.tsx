64e452387b5092061a5fa002905f574c809cadda
lucasGabrielDeAA <lg.technus@gmail.com>
2020-04-20 16:45:43 -0300

    styles added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Router } from "react-router";
+import { ToastContainer } from 'react-toastify';
+
+import "./styles/toast.css";
 
 import history from "./services/history";
 
@@ -9,6 +12,7 @@ export default function App() {
   return (
     <Router history={history}>
       <Routes />
+      <ToastContainer autoClose={3000} />
     </Router>
   )
 }
