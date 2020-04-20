bb576d187150295ea3b0fa7645fc16f845c332b0
lucasGabrielDeAA <lg.technus@gmail.com>
2020-04-20 15:58:30 -0300

    db.json updated

--- src/pages/CoursesDetail.tsx
+++ src/pages/CoursesDetail.tsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import urlParser from "../common/urlParser";
 import api from "../services/api";
 
-import IUser from "../types/IUser";
 import ICourse from "../types/ICourse";
 
 interface Props {
@@ -22,14 +21,12 @@ const CoursesDetail: React.FC<Props> = ({ history, location }) => {
     date_start: new Date(),
     users: [],
   });
-  const [users, setUsers] = useState<Array<IUser>>([]);
 
   useEffect(() => {
     async function loadData() {
       const { data } = await api.get(`/courses/${id}`);
 
       setCourse(data);
-      setUsers(data.users);
     }
 
     loadData();
@@ -47,18 +44,20 @@ const CoursesDetail: React.FC<Props> = ({ history, location }) => {
   }
 
   function handleAddUser() {
-    setUsers([ ...users, { id: String(Math.round(Math.random() * 1000)), age: 0, email: '', name: '' }]);
+    setCourse({ ...course, users: [...course.users, { id: String(Math.round(Math.random() * 1000)), age: 0, email: '', name: '' }] });
   }
 
   function handleChangeUser(user_id: string, name: string, value: any) {
-    setUsers(users.map(user => {
-      if (user.id === user_id) {
-        user = { ...user, [name]: value};
-        return user;
-      } else {
-        return user;
-      }
-    }));
+    setCourse({
+      ...course, users: course.users.map(user => {
+        if (user.id === user_id) {
+          user = { ...user, [name]: value };
+          return user;
+        } else {
+          return user;
+        }
+      })
+    });
   }
 
   return (
@@ -68,15 +67,13 @@ const CoursesDetail: React.FC<Props> = ({ history, location }) => {
         <input type="number" value={course.duration} onChange={e => handleChangeCourse('duration', e.target.value)} />
 
 
-        <ul>
-          {users.map(user => (
-            <div key={user.id}>
-              <input type="text" value={user.name} onChange={e => handleChangeUser(user.id, "name", e.target.value)} />
-              <input type="email" value={user.email} onChange={e => handleChangeUser(user.id, "email", e.target.value)} />
-              <input type="number" value={user.age} onChange={e => handleChangeUser(user.id, "age", e.target.value)} />
-            </div>
-          ))}
-        </ul>
+        {course.users.map(user => (
+          <div key={user.id}>
+            <input type="text" value={user.name} onChange={e => handleChangeUser(user.id, "name", e.target.value)} />
+            <input type="email" value={user.email} onChange={e => handleChangeUser(user.id, "email", e.target.value)} />
+            <input type="number" value={user.age} onChange={e => handleChangeUser(user.id, "age", e.target.value)} />
+          </div>
+        ))}
 
         <button type="button" onClick={() => handleAddUser()}>+ User</button>
         <button type="submit">Save</button>
